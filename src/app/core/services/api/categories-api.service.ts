07f157370fb27e92a68b1aab9350c18ffd710db9
olegkuibar <kuibar.oleg@gmail.com>
2019-09-27 20:39:55 +0300

    feat(LEAN-99): update api endpoints

--- src/app/core/services/api/categories-api.service.ts
+++ src/app/core/services/api/categories-api.service.ts
@@ -12,22 +12,22 @@ export class CategoriesApiService {
   constructor(private http: HttpClient) {}
 
   getCategories(): Promise<Category[]> {
-    return this.http.get<Category[]>(`${environment.apiUrl}/categories`).toPromise();
+    return this.http.get<Category[]>(`${environment.apiUrl}/categories/tree`).toPromise();
   }
 
   createTree(node: CategoryNode[]): Promise<boolean> {
-    return this.http.post<boolean>(`${environment.apiUrl}/categories`, node).toPromise();
+    return this.http.post<boolean>(`${environment.apiUrl}/categories/tree`, node).toPromise();
   }
 
   getTree(id: string): Promise<CategoryNode[]> {
-    return this.http.get<CategoryNode[]>(`${environment.apiUrl}/categories/${id}/tree`).toPromise();
+    return this.http.get<CategoryNode[]>(`${environment.apiUrl}/categories/tree/${id}`).toPromise();
   }
 
   updateTree(id: string, node: CategoryNode[]): Promise<boolean> {
-    return this.http.put<boolean>(`${environment.apiUrl}/categories/${id}/tree`, node).toPromise();
+    return this.http.put<boolean>(`${environment.apiUrl}/categories/tree/${id}`, node).toPromise();
   }
 
   updateTreeNode(id: string, nodeId: number, node: CategoryNode[]): Promise<boolean> {
-    return this.http.put<boolean>(`${environment.apiUrl}/categories/${id}/tree/${nodeId}`, node).toPromise();
+    return this.http.put<boolean>(`${environment.apiUrl}/categories/tree/${id}/${nodeId}`, node).toPromise();
   }
 }
